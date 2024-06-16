6a275b7b1fe3ff7172753f597701aa09b732a7de
mearvinddevda <arvinddevda02@gmail.com>
2024-06-16 12:12:07 +0530

    Backend Modification

---
+++ BackEnd/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+const app= express();
+dotenv.config();
+
+const PORT = process.env.PORT || 4001 ;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+}) 
